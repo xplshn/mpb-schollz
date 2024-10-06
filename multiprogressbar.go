4ebde08d16ff9125d55be37b3e801e2adc423283
xplshn <114888778+xplshn@users.noreply.github.com>
2024-10-06 04:13:07 -0300

    Update multiprogressbar.go

--- multiprogressbar.go
+++ multiprogressbar.go
@@ -1,6 +1,7 @@
 package multiprogressbar
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
@@ -9,24 +10,26 @@ import (
 	"github.com/schollz/progressbar/v3"
 )
 
+// MultiProgressBar manages multiple progress bars.
 type MultiProgressBar struct {
 	curLine int
 	bars    []*progressbar.ProgressBar
 	guard   sync.Mutex
-	output  io.Writer
+	output  *bufio.Writer
 }
 
+// New creates a new MultiProgressBar with default options.
 func New() *MultiProgressBar {
 	return NewOptions()
 }
 
-// NewOptions creates a new multi progressbar with passed options.
+// NewOptions creates a new MultiProgressBar with the provided options.
 func NewOptions(options ...Option) *MultiProgressBar {
 	mpb := &MultiProgressBar{
 		curLine: 0,
 		bars:    []*progressbar.ProgressBar{},
 		guard:   sync.Mutex{},
-		output:  os.Stdout,
+		output:  bufio.NewWriter(os.Stdout),
 	}
 	for _, opt := range options {
 		opt(mpb)
@@ -34,10 +37,10 @@ func NewOptions(options ...Option) *MultiProgressBar {
 	return mpb
 }
 
-// Add a progress bar. This will change the writer of the progress bar.
-// Do not change the writer afterwards!
+// Add adds a progress bar to the MultiProgressBar.
+// This changes the writer of the progress bar. Do not change the writer afterwards!
 // Not thread safe.
-// Return the passed progress bar.
+// Returns the added progress bar.
 func (mpb *MultiProgressBar) Add(pBar *progressbar.ProgressBar) *progressbar.ProgressBar {
 	progressbar.OptionSetWriter(&multiProgressBarWriter{
 		MultiProgressBar: mpb,
@@ -47,22 +50,22 @@ func (mpb *MultiProgressBar) Add(pBar *progressbar.ProgressBar) *progressbar.Pro
 	return pBar
 }
 
-// Get returns the progressbar.Progressbar with the given index.
-// Will panic if the index does not exist.
+// Get returns the progressbar.ProgressBar at the given index.
+// Panics if the index does not exist.
 func (mpb *MultiProgressBar) Get(idx int) *progressbar.ProgressBar {
 	return mpb.bars[idx]
 }
 
+// BarCount returns the number of progress bars.
 func (mpb *MultiProgressBar) BarCount() int {
 	return len(mpb.bars)
 }
 
 // RenderBlank calls RenderBlank on all progress bars.
-// If an error is thrown, RenderBlank might not be called on all bars.
+// If an error occurs, RenderBlank might not be called on all bars.
 func (mpb *MultiProgressBar) RenderBlank() error {
 	for _, pbar := range mpb.bars {
-		err := pbar.RenderBlank()
-		if err != nil {
+		if err := pbar.RenderBlank(); err != nil {
 			return err
 		}
 	}
@@ -70,26 +73,28 @@ func (mpb *MultiProgressBar) RenderBlank() error {
 }
 
 // Finish calls Finish on all progress bars.
-// If an error is thrown, Finish might not be called on all bars.
-// This will also call End.
+// If an error occurs, Finish might not be called on all bars.
+// This also calls End.
 func (mpb *MultiProgressBar) Finish() error {
 	for _, pbar := range mpb.bars {
-		err := pbar.Finish()
-		if err != nil {
+		if err := pbar.Finish(); err != nil {
 			return err
 		}
 	}
 	return mpb.End()
 }
 
-// End Move cursor to the end of the progress bars.
+// End moves the cursor to the end of the progress bars.
 // Not thread safe.
 func (mpb *MultiProgressBar) End() error {
 	_, err := mpb.move(len(mpb.bars), mpb.output)
-	return err
+	if err != nil {
+		return err
+	}
+	return mpb.output.Flush()
 }
 
-// Move cursor to the beginning of the current progressbar.
+// move moves the cursor to the beginning of the current progress bar.
 func (mpb *MultiProgressBar) move(id int, writer io.Writer) (int, error) {
 	bias := mpb.curLine - id
 	mpb.curLine = id
@@ -103,20 +108,18 @@ func (mpb *MultiProgressBar) move(id int, writer io.Writer) (int, error) {
 	return 0, nil
 }
 
-// personal note about Option. I do not like this pattern, but i will use it nonetheless, to stay close to schollz/progressbar.
-
-// Option is the type all options need to adhere to
+// Option is the type all options need to adhere to.
 type Option func(p *MultiProgressBar)
 
 // OptionSetWriter sets the output writer.
-// Unknown behaviour if called while using the multi progress bar.
+// Behavior is undefined if called while using the MultiProgressBar.
 func OptionSetWriter(writer io.Writer) Option {
 	return func(mpb *MultiProgressBar) {
-		mpb.output = writer
+		mpb.output = bufio.NewWriter(writer)
 	}
 }
 
-// io.Writer wrapper to know which progressbar wants to write.
+// multiProgressBarWriter is an io.Writer wrapper to know which progress bar wants to write.
 type multiProgressBarWriter struct {
 	*MultiProgressBar
 	idx int
